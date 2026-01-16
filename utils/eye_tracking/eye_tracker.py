cc650cab71ec87873995136b325d0a8e7b30b407
Ryan Barnard <barnardr98@gmail.com>
2026-01-15 22:23:59 -0500

    Update eye tracker

--- utils/eye_tracking/eye_tracker.py
+++ utils/eye_tracking/eye_tracker.py
@@ -37,7 +37,7 @@ class EyeTracker:
 
         # Logic Variables
         self.baseline_ratio = None
-        self.threshold_buffer = 0.05
+        self.threshold_buffer = 0.02
         self.eyes_off = False
         
         self.history_size = 12
